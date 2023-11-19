9eb0ad7674178a5130849684a4f54e379cf085be
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-11-20 01:35:03 +0200

    add chip storage

--- HW-18-Threads.playground/Contents.swift
+++ HW-18-Threads.playground/Contents.swift
@@ -22,3 +22,37 @@ public struct Chip {
         sleep(UInt32(soderingTime))
     }
 }
+
+// MARK: Chip Storage
+
+class ChipStorage {
+    var storage = [Chip]()
+    var mutex = NSCondition()
+    private var count = Int()
+    var isAvailable = Bool()
+    
+    var isEmpty: Bool {
+        storage.isEmpty
+    }
+    
+    func add(chip: Chip) {
+        mutex.lock()
+        isAvailable = true
+        storage.append(chip)
+        count += 1
+        print("Chip #\(count) added in storage!")
+        mutex.signal()
+        mutex.unlock()
+    }
+    
+    func remove() -> Chip {
+        mutex.lock()
+        while !isAvailable {
+            mutex.wait()
+            print("Waiting...")
+        }
+        isAvailable = false
+        mutex.unlock()
+        return storage.removeLast()
+    }
+}
